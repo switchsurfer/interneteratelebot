8a98c9c02306c7c9432ac09a1978bf2111c7bad1
1 <a1@MacBook-Pro-1.ricentr-isp.ru>
2018-11-06 18:29:14 +0300

    setup

--- main.py
+++ main.py
@@ -37,15 +37,43 @@ async def on_startup(dp):
 async def on_shutdown(dp):
     await bot.delete_webhook()
 
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+
+@dp.message_handler(content_types=ContentType.ANY)
+async def unknown_message(msg: types.Message):
+    message_text = text(emojize('Я не знаю, что с этим делать :astonished:'),
+                        italic('\nЯ просто напомню,'), 'что есть',
+                        code('команда'), '/help')
+    await msg.reply(message_text, parse_mode=ParseMode.MARKDOWN)
+
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
+    await message.reply('Привет!\nИспользуй /help, '
+                        'чтобы узнать список доступных команд!')
 
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+    msg = text(bold('Я могу ответить на следующие команды:'),
+               '/voice', '/photo', '/group', '/note', '/file, /testpre', sep='\n')
+    await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
+
+@dp.message_handler(commands=['voice'])
+async def process_voice_command(message: types.Message):
+    await bot.send_voice(message.from_user.id, VOICE,
+                        reply_to_message_id=message.message_id)
+
+@dp.message_handler(commands=['photo'])
+async def process_photo_command(message: types.Message):
+    caption = 'Какие глазки! :eyes:'
+    await bot.send_photo(message.from_user.id, CAT_BIG_EYES,
+                         caption=emojize(caption),
+                         reply_to_message_id=message.message_id)
+
 
 
 if __name__ == '__main__':
