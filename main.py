cc06931fb568852a7a36294f69def7b0c8b65296
1 <a1@MacBook-Pro-1.ricentr-isp.ru>
2018-11-07 16:54:00 +0300

    setup

--- main.py
+++ main.py
@@ -159,8 +159,10 @@ async def process_help_command(message: types.Message):
 #     await bot.send_message(msg.from_user.id, msg.text)
 
 
-help_message = text(
-    "Это урок по клавиатурам.",
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Это урок по клавиатурам.",
     "Доступные команды:\n",
     "/start - приветствие",
     "\nШаблоны клавиатур:",
@@ -175,12 +177,7 @@ help_message = text(
     "\nИнлайн клавиатуры:",
     "/1 - первая кнопка",
     "/2 - сразу много кнопок",
-    sep="\n"
-)
-
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.reply(help_message)
+    sep="\n")
 
 
 
