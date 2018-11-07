6d00897f338561a4df7a8d5cac91e37c5ed4af63
1 <a1@MacBook-Pro-1.ricentr-isp.ru>
2018-11-07 10:55:24 +0300

    setup

--- main.py
+++ main.py
@@ -46,18 +46,17 @@ async def process_help_command(message: types.Message):
     await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
 
 
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text)
+# @dp.message_handler()
+# async def echo_message(msg: types.Message):
+#     await bot.send_message(msg.from_user.id, msg.text)
 
 
-@dp.message_handler(content_types=ContentType.ANY)
-async def unknown_message(msg: types.Message):
-    message_text = text(emojize('Я не знаю, что с этим делать :astonished:'),
-                        italic('\nЯ просто напомню,'), 'что есть',
-                        code('команда'), '/help')
-    await msg.reply(message_text, parse_mode=ParseMode.MARKDOWN)
-
+# @dp.message_handler(content_types=ContentType.ANY)
+# async def unknown_message(msg: types.Message):
+#     message_text = text(emojize('Я не знаю, что с этим делать :astonished:'),
+#                         italic('\nЯ просто напомню,'), 'что есть',
+#                         code('команда'), '/help')
+#     await msg.reply(message_text, parse_mode=ParseMode.MARKDOWN)
 
 
 
