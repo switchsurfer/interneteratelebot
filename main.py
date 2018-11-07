3ce5fa91be2529503c111f33ecf0cdbb26a2f6d4
1 <a1@MacBook-Pro-1.ricentr-isp.ru>
2018-11-07 16:22:10 +0300

    setup

--- main.py
+++ main.py
@@ -34,9 +34,91 @@ async def on_startup(dp):
 async def on_shutdown(dp):
     await bot.delete_webhook()
 
+
+###
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply("Привет!", reply_markup=kb.greet_kb)
+
+
+@dp.message_handler(commands=['hi1'])
+async def process_hi1_command(message: types.Message):
+    await message.reply("Первое - изменяем размер клавиатуры",
+                        reply_markup=kb.greet_kb1)
+
+
+@dp.message_handler(commands=['hi2'])
+async def process_hi2_command(message: types.Message):
+    await message.reply("Второе - прячем клавиатуру после одного нажатия",
+                        reply_markup=kb.greet_kb2)
+
+
+@dp.message_handler(commands=['hi3'])
+async def process_hi3_command(message: types.Message):
+    await message.reply("Третье - добавляем больше кнопок",
+                        reply_markup=kb.markup3)
+
+
+@dp.message_handler(commands=['hi4'])
+async def process_hi4_command(message: types.Message):
+    await message.reply("Четвертое - расставляем кнопки в ряд",
+                        reply_markup=kb.markup4)
+
+
+@dp.message_handler(commands=['hi5'])
+async def process_hi5_command(message: types.Message):
+    await message.reply("Пятое - добавляем ряды кнопок",
+                        reply_markup=kb.markup5)
+
+
+@dp.message_handler(commands=['hi6'])
+async def process_hi6_command(message: types.Message):
+    await message.reply("Шестое - запрашиваем контакт и геолокацию\n"
+                        "Эти две кнопки не зависят друг от друга",
+                        reply_markup=kb.markup_request)
+
+
+@dp.message_handler(commands=['hi7'])
+async def process_hi7_command(message: types.Message):
+    await message.reply("Седьмое - все методы вместе",
+                        reply_markup=kb.markup_big)
+
+
+@dp.message_handler(commands=['rm'])
+async def process_rm_command(message: types.Message):
+    await message.reply("Убираем шаблоны сообщений",
+                        reply_markup=kb.ReplyKeyboardRemove())
+
+@dp.message_handler(commands=['1'])
+async def process_command_1(message: types.Message):
+    await message.reply("Первая инлайн кнопка",
+                        reply_markup=kb.inline_kb1)
+
+
+@dp.message_handler(commands=['2'])
+async def process_command_2(message: types.Message):
+    await message.reply("Отправляю все возможные кнопки",
+                        reply_markup=kb.inline_kb_full)
+
+help_message = text(
+    "Это урок по клавиатурам.",
+    "Доступные команды:\n",
+    "/start - приветствие",
+    "\nШаблоны клавиатур:",
+    "/hi1 - авто размер",
+    "/hi2 - скрыть после нажатия",
+    "/hi3 - больше кнопок",
+    "/hi4 - кнопки в ряд",
+    "/hi5 - больше рядов",
+    "/hi6 - запрос локации и номера телефона",
+    "/hi7 - все методы"
+    "/rm - убрать шаблоны",
+    "\nИнлайн клавиатуры:",
+    "/1 - первая кнопка",
+    "/2 - сразу много кнопок",
+    sep="\n"
+)
+###
 
 
 @dp.message_handler(commands=['help'])
