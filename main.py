9397ff605f967046d26a80d275f701cf70dfee66
1 <a1@MacBook-Pro-1.ricentr-isp.ru>
2018-11-07 10:46:32 +0300

    setup

--- main.py
+++ main.py
@@ -25,29 +25,12 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
-#@dp.message_handler()
-#async def echo(message: types.Message):
-#    await bot.send_message(message.chat.id, message.text)
-
-
 async def on_startup(dp):
     await bot.set_webhook(WEBHOOK_URL)
 
 
 async def on_shutdown(dp):
     await bot.delete_webhook()
-
-#@dp.message_handler()
-#async def echo_message(msg: types.Message):
-#    await bot.send_message(msg.from_user.id, msg.text)
-
-
-#@dp.message_handler(content_types=ContentType.ANY)
-#async def unknown_message(msg: types.Message):
-#    message_text = text(emojize('Я не знаю, что с этим делать :astonished:'),
-#                        italic('\nЯ просто напомню,'), 'что есть',
-#                        code('команда'), '/help')
-#    await msg.reply(message_text, parse_mode=ParseMode.MARKDOWN)
 
 
 @dp.message_handler(commands=['start'])
@@ -62,17 +45,19 @@ async def process_help_command(message: types.Message):
                '/voice', '/photo', '/group', '/note', '/file, /testpre', sep='\n')
     await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
 
-@dp.message_handler(commands=['voice'])
-async def process_voice_command(message: types.Message):
-    await bot.send_voice(message.from_user.id, VOICE,
-                        reply_to_message_id=message.message_id)
 
-@dp.message_handler(commands=['photo'])
-async def process_photo_command(message: types.Message):
-    caption = 'Какие глазки! :eyes:'
-    await bot.send_photo(message.from_user.id, CAT_BIG_EYES,
-                         caption=emojize(caption),
-                         reply_to_message_id=message.message_id)
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+
+@dp.message_handler(content_types=ContentType.ANY)
+async def unknown_message(msg: types.Message):
+    message_text = text(emojize('Я не знаю, что с этим делать :astonished:'),
+                        italic('\nЯ просто напомню,'), 'что есть',
+                        code('команда'), '/help')
+    await msg.reply(message_text, parse_mode=ParseMode.MARKDOWN)
+
 
 
 
