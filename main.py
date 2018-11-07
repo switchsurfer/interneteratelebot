9bc42390761c051313e4f9f9538c94510cc85b5c
1 <a1@MacBook-Pro-1.ricentr-isp.ru>
2018-11-07 10:59:56 +0300

    setup

--- main.py
+++ main.py
@@ -21,16 +21,18 @@ WEBAPP_PORT = os.environ.get('PORT')
 
 logging.basicConfig(level=logging.INFO)
 
+
+
+
+# async def on_startup(dp):
+#     await bot.set_webhook(WEBHOOK_URL)
+
+
+# async def on_shutdown(dp):
+#     await bot.delete_webhook()
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-
-
-async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
-
-
-async def on_shutdown(dp):
-    await bot.delete_webhook()
 
 
 @dp.message_handler(commands=['start'])
@@ -44,6 +46,7 @@ async def process_help_command(message: types.Message):
     msg = text(bold('Я могу ответить на следующие команды:'),
                '/voice', '/photo', '/group', '/note', '/file, /testpre', sep='\n')
     await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
+
 
 
 # @dp.message_handler()
@@ -61,6 +64,7 @@ async def process_help_command(message: types.Message):
 
 
 if __name__ == '__main__':
-    start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
-                  on_startup=on_startup, on_shutdown=on_shutdown,
-                  skip_updates=True, host=WEBAPP_HOST, port=WEBAPP_PORT)
+    executor.start_polling(dp)
+    # start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
+    #               on_startup=on_startup, on_shutdown=on_shutdown,
+    #               skip_updates=True, host=WEBAPP_HOST, port=WEBAPP_PORT)
